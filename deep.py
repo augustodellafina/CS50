e08e441c6080c500e512741f2ab39c4169abb7e0
augustodellafina <augustodellafina@users.noreply.github.com>
2023-09-20 03:40:49 +0100

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -7,4 +7,4 @@ user_input = input("What is the Answer to the Great Question of Life, the Univer
 if user_input.lower() == "42" or user_input.lower() == "forty-two" or user_input.lower() == "forty two":
     print("Yes")
 else:
-    print("No")
+    print("No\n")
