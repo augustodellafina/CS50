289787b0cc83cb410c23aca12f6438aac966fec6
augustodellafina <augustodellafina@users.noreply.github.com>
2023-09-23 23:48:01 +0100

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -1,25 +1,27 @@
-# Initialize variables to keep track of the total amount and accepted coin denominations
+# Initialize variables to keep track of the total amount and accepted coins
 total_amount = 0
 amount_due = 50
 accepted_coins = [25, 10, 5]
 
 print(f"Amount Due: {amount_due}")
-# Continue looping until the total amount reaches 50 cents or more
+# Continue looping until the total amount reaches 50 cents
 while total_amount < 50:
     try:
         # Prompt the user for a coin input and convert it to an integer
         coin = int(input("Insert Coin: "))
         
-        # Check if the coin is an accepted denomination
+        # Check if the coin is accepted
         if coin in accepted_coins:
             amount_due -= coin
             total_amount += coin
             if amount_due > 0:
                 print(f"Amount Due: {amount_due}")
         else:
-            print(f"Amount Due: {amount_due}")
+            # Prompt user to input a valid amount
             print("Invalid coin. Please insert a 25, 10, or 5 cent coin.")
+            print(f"Amount Due: {amount_due}")
     except ValueError:
+        # Check if the input is a number
         print("Invalid coin. Please enter a valid coin.")
 
 # Calculate and display the change owed
