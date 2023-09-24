b93597979fa837fcb9c74e0327c6e61966104ddb
augustodellafina <augustodellafina@users.noreply.github.com>
2023-09-24 02:55:41 +0100

    automated commit by check50 [check50=True]

--- plates.py
+++ plates.py
@@ -5,27 +5,24 @@ def main():
     else:
         print("Invalid")
 
+def starts_with_letters(s):
+    # Check if the first two characters are letters
+    return s[:2].isalpha()
+
+def has_valid_length(s):
+    # Check if the length of the string is between 2 and 6 characters
+    return 2 <= len(s) <= 6
+
+def no_invalid_numbers(s):
+    # Check if the first character is not '0', and there are no numbers in the middle
+    return s[0] != '0' and all(char.isalpha() for char in s[2:-1])
+
+def has_numbers_at_end(s):
+    # Check if the last character is a number
+    return s[-1].isdigit()
+
 def is_valid(s):
-    # Requirement 1: Plate must have at least 2 letters at the beginning
-    if len(s) < 2 or not s[:2].isalpha():
-        return False
-
-    # Requirement 2: Plate must have a maximum of 6 characters
-    if len(s) > 6:
-        return False
-
-    # Requirement 3: Numbers cannot be in the middle, and the first number cannot be 0
-    if s[2:].isdigit():
-        if s[2] == '0':
-            return False
-        if not s[2:].isnumeric():
-            return False
-
-    # Requirement 4: No periods, spaces, or punctuation marks are allowed
-    if any(char in ". ,;:!?" for char in s):
-        return False
-
-    # If all requirements are met, return True
-    return True
+    # Check all the requirements
+    return starts_with_letters(s) and has_valid_length(s) and no_invalid_numbers(s) and has_numbers_at_end(s)
 
 main()
