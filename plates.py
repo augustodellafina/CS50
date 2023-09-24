f6228759fdc1de466c6f4e2081327a7bc25cf574
augustodellafina <augustodellafina@users.noreply.github.com>
2023-09-24 03:09:54 +0100

    automated commit by check50 [check50=True]

--- plates.py
+++ plates.py
@@ -12,15 +12,21 @@ def starts_with_letters(s):
     return first_two.isalpha()
 
 def has_valid_numbers(s):
-    if s[-1] == '0':
-        return False
-    for i in range(len(s) - 1):
-        if s[i].isdigit() and not s[i + 1].isdigit():
-            return False
+    if not s[0].isalpha():
+        return False  # Check if the first character is a letter
+    first_digit = None
+    for i, char in enumerate(s):
+        if char.isdigit():
+            if first_digit is None:
+                first_digit = char
+            if first_digit == '0':
+                return False  # The first number is '0', which is invalid
+        elif first_digit is not None:
+            break  # We found a non-digit character after the first number
     return True
 
 def has_valid_length(s):
-    return 2 < len(s) > 6
+    return 2 <= len(s) <= 6
 
 def has_no_punctuation(s):
     return all(char.isalnum() for char in s)
